498e44a7739f460cc3528fa5be781a9d2ce1f057
patrickhyena <56729438+patrickhyena@users.noreply.github.com>
2019-12-18 10:24:24 +0700

    part two question one

--- quiz part two question one.py
+++ quiz part two question one.py
@@ -12,19 +12,30 @@ class Spell:
 class Accio(Spell):
     def __init__(self):
         Spell.__init__(self, 'Accio', 'Summoning Charm')
+    #edit
     def get_description(self):
         return 'This charm summons an object to the caster, potentially over a significant distance'
-
+    #end of edit
+    
 class Confundo(Spell):
     def __init__(self):
         Spell.__init__(self, 'Confundo', 'Confundus Charm')
     def get_description(self):
         return 'Causes the victim to become confused and befuddled.'
-    def study_spell(spell):
-        print(spell)
 
+#edit
+def study_spell(spell):
+        print(str('Studied the ') + str(spell) + str(' spell'))
+#end of edit
+
+spell = Accio()
+spell.execute()
+#edit
+study_spell('Accio')
+study_spell("Confundo")
 print(Accio())
+#end of edit
 
 #a : Parent = Spell, child = Accio, Confundo
 #b : an error
-#c : it tried to call the function study_spell if it was outside of a class as we didnt specify it with the class identifier
+#c : none, there is no get description method when using this function
